281ddd562eca973e930f7bd60f30a108e6ba7051
Nolee <norris.bravo@gmail.com>
2021-06-22 01:14:22 -0500

    version 5.0

--- sw.js
+++ sw.js
@@ -17,7 +17,6 @@ const CACHE_NAME = 'V1_cache_Calculator_NoleeBH',
     './img/calculator_1024.png'
   ]
 
- //durante la fase de instalación, generalmente se almacena en caché los activos estáticos
 self.addEventListener('install', e => {
   e.waitUntil(
     caches.open(CACHE_NAME)
@@ -29,7 +28,6 @@ self.addEventListener('install', e => {
   )
 })
 
-//una vez que se instala el SW, se activa y busca los recursos para hacer que funcione sin conexión
 self.addEventListener('activate', e => {
   const cacheWhitelist = [CACHE_NAME]
 
@@ -38,29 +36,23 @@ self.addEventListener('activate', e => {
       .then(cacheNames => {
         return Promise.all(
           cacheNames.map(cacheName => {
-            //Eliminamos lo que ya no se necesita en cache
             if (cacheWhitelist.indexOf(cacheName) === -1) {
               return caches.delete(cacheName)
             }
           })
         )
       })
-      // Le indica al SW activar el cache actual
       .then(() => self.clients.claim())
   )
 })
 
-//cuando el navegador recupera una url
 self.addEventListener('fetch', e => {
-  //Responder ya sea con el objeto en caché o continuar y buscar la url real
   e.respondWith(
     caches.match(e.request)
       .then(res => {
         if (res) {
-          //recuperar del cache
           return res
         }
-        //recuperar de la petición a la url
         return fetch(e.request)
       })
   )

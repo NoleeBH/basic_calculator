cb9d4e668fab7cd70414fa9b1c6ce90ad40f2bb8
Nolee <norris.bravo@gmail.com>
2021-06-22 00:10:46 -0500

    version 3.0 WPA

--- js/css-dynamic.js
+++ js/css-dynamic.js
@@ -1,4 +1,9 @@
 
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('./sw.js')
+      .then(reg => console.log('Successful SW registration', reg))
+      .catch(err => console.warn('Error registering sw', err))
+  }
 colorBody = fondoColor => document.body.style.backgroundColor = fondoColor;
 
 colorDiv = fondoColor => {
